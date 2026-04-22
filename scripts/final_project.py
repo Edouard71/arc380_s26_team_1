ac511ed88cac840406be1eb759ca601276babaf2
AChang202 <ac7713@princeton.edu>
2026-04-21 20:54:58 -0400

    testing with AI

--- scripts/final_project.py
+++ scripts/final_project.py
@@ -1546,7 +1546,7 @@ def main():
     indices_within_radius = np.where(sorted_scattered_distances <= farthest_distance + r_buffer)[0]
     indices_outside_radius = np.where(sorted_scattered_distances > farthest_distance + r_buffer)[0]
 
-    num_blocks_arr = np.ones(len(out_blocks))
+    num_blocks_arr = np.ones(len(scattered_pos))
 
     in_blocks = [sorted_scattered_block_array[i] for i in indices_within_radius]
     out_blocks = [sorted_scattered_block_array[i] for i in indices_outside_radius]
